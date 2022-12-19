1121222d76ce05a94707f37b5bc6f3269cfe7426
jhades <jhades.dev@gmail.com>
2022-12-19 11:55:21 +0100

    preparing course

--- backend/get-courses.route.ts
+++ backend/get-courses.route.ts
@@ -7,7 +7,7 @@ import {COURSES} from "./db-data";
 
 export function getAllCourses(req: Request, res: Response) {
 
-  res.status(200).json({payload:Object.values(COURSES).sort((c1:any, c2:any) => c1.seqNo - c2.seqNo)});
+  res.status(200).json(Object.values(COURSES).sort((c1:any, c2:any) => c1.seqNo - c2.seqNo));
 
 }
 
--- src/routes/courses/index.tsx
+++ src/routes/courses/index.tsx
@@ -1,14 +1,31 @@
-import { component$, useStylesScoped$ } from "@builder.io/qwik";
+import { component$, useStylesScoped$, Resource } from "@builder.io/qwik";
 import styles from './courses.css?inline';
-import type { DocumentHead } from '@builder.io/qwik-city';
+import type { DocumentHead, RequestHandler } from "@builder.io/qwik-city";
+import { Course } from "~/models/course";
+import { useEndpoint } from "@builder.io/qwik-city";
 
+export const onGet: RequestHandler<Course[]> = async () => {
+  const response = await fetch(`http://localhost:9000/api/courses`);
+  return response.json();
+};
 
 export default component$(() => {
 
   useStylesScoped$(styles);
 
+  const courseData = useEndpoint<Course[]>();
+
   return (
-    <h1>Hello World Courses!</h1>
+    <Resource
+      value={courseData}
+      onPending={() => <div>Loading...</div>}
+      onRejected={() => <div>Error</div>}
+      onResolved={(courses) => (
+        <>
+          <h1>Courses: {courses.length}</h1>
+        </>
+      )}
+    />
   );
 });
 

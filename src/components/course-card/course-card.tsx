a84b2b1d98c5c352e47520e3938adb64d867bb87
jhades <jhades.dev@gmail.com>
2022-12-20 11:56:45 +0100

    preparing course

--- src/components/course-card/course-card.tsx
+++ src/components/course-card/course-card.tsx
@@ -1,4 +1,4 @@
-import { component$, useStylesScoped$ } from "@builder.io/qwik";
+import { component$, useStore, useStylesScoped$ } from "@builder.io/qwik";
 import styles from './course-card.css?inline';
 import { Course } from "~/models/course";
 
@@ -10,17 +10,26 @@ export default component$( (props:CourseCardProps) => {
 
   useStylesScoped$(styles);
 
+  const store = useStore({course: props.course}, {recursive: true});
+
+  const {course} = store;
+
   return (
   <div class="course-card">
-    <img class='card-image' src={props.course.iconUrl}/>
-    <div class='card-title'>{props.course.description}</div>
-    <div class='card-description'>{props.course.longDescription}</div>
+    <img class='card-image' src={course.iconUrl}/>
+    <div class='card-title'>{course.description}</div>
+    <div class='card-description'>{course.longDescription}</div>
     <div class='card-actions'>
         <button>View</button>
-        <button>Edit</button>
+        <button onClick$={() => onCourseEdited(course)}>Edit</button>
     </div>
 
   </div>
   );
 
 });
+
+export function onCourseEdited(course: Course) {
+  console.log(`Hello World`);
+  course.description = `Edited: ${course.description}`;
+}

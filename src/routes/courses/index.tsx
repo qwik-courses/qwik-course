f5155d7d90b3c26bbd63bfbb42753e3cf58e93e0
jhades <jhades.dev@gmail.com>
2022-12-20 15:50:45 +0100

    preparing course

--- src/routes/courses/index.tsx
+++ src/routes/courses/index.tsx
@@ -1,4 +1,4 @@
-import { component$, useStylesScoped$, Resource } from "@builder.io/qwik";
+import { component$, useStylesScoped$, Resource, useResource$ } from "@builder.io/qwik";
 import styles from "./courses.css?inline";
 import type { DocumentHead } from "@builder.io/qwik-city";
 import { Course } from "~/models/course";
@@ -7,22 +7,17 @@ import CourseCardList from "~/components/course-card-list/course-card-list";
 import { commonLinks } from "~/routes/head-links";
 
 
-
-export const onGet: RequestHandler<Course[]> = async () => {
-  const response = await fetch(`http://localhost:9000/api/courses`);
-  return response.json();
-};
-
-
 export default component$(() => {
 
   useStylesScoped$(styles);
 
-  const courseData = useEndpoint<Course[]>();
+  const resource = useResource$<Course[]>(async () => {
+    return getCourses();
+  });
 
   return (
     <Resource
-      value={courseData}
+      value={resource}
       onPending={() => <div>Loading...</div>}
       onRejected={() => <div>Error</div>}
       onResolved={(courses) => (
@@ -38,3 +33,8 @@ export const head: DocumentHead = {
   title: "Courses Page",
   links: commonLinks
 };
+
+export async function getCourses() {
+  const response = await fetch(`http://localhost:9000/api/courses`);
+  return response.json();
+}

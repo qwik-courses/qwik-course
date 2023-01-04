65f8017c1210e8589794647987b844c91a9c28c3
jhades <jhades.dev@gmail.com>
2023-01-04 10:22:59 +0100

    prepare course

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,22 +1,2 @@
-import { component$ } from '@builder.io/qwik';
-import type { DocumentHead } from '@builder.io/qwik-city';
-import { Link } from '@builder.io/qwik-city';
-import { commonLinks } from "~/routes/head-links";
 
-export default component$(() => {
-  return (
-    <div>
-      <h1>Welcome to this Qwik Course!</h1>
-    </div>
-  );
-});
 
-export const head: DocumentHead = {
-  title: 'Qwik In Depth Course',
-  links: commonLinks,
-  meta: [
-    {
-      name: 'description'
-    },
-  ],
-};

2a12944c08d968c24ddb05fcd93b97bc1ee7dc59
jhades <jhades.dev@gmail.com>
2023-01-10 17:46:57 +0100

    Recording Qwik course

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,2 +1,15 @@
+import { component$ } from "@builder.io/qwik";
 
+
+export default component$(() => {
+
+  console.log(`Initializing Hello World component.`);
+
+  return (
+  <h1>
+    Welcome to this Qwik Course!
+  </h1>
+  );
+
+});
 

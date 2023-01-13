4de324207f30b9a34302e502a22d71ed62ed76f4
jhades <jhades.dev@gmail.com>
2023-01-13 14:42:24 +0100

    Recording Qwik course

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, $ } from "@builder.io/qwik";
 import { HelloMessage } from "~/components/hello-message/hello-message";
 
 
@@ -6,15 +6,23 @@ export default component$(() => {
 
   console.log(`Initializing Hello World component.`);
 
+  const sayHello = $(() => {
+    alert("Hello World!");
+  });
+
   return (
     <>
       <HelloMessage message="Hello World" courseVersion={1} />
       <HelloMessage message="Welcome to this Qwik Course" courseVersion={2} />
       <HelloMessage message="Learn the Qwik Framework!"  />
+
+      <button onClick$={sayHello}>Say Hello</button>
+
     </>
   );
 
 });
+
 
 
 

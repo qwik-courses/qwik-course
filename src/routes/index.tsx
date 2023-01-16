497a337a77976ae95707b9a735cf1129b1ddb0d9
jhades <jhades.dev@gmail.com>
2023-01-16 17:13:26 +0100

    Recording Qwik course

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -10,13 +10,19 @@ export default component$(() => {
     alert("Hello World!");
   });
 
+  const messages = [
+    "Hello World",
+    "Welcome to this Qwik Course",
+    "Learn the Qwik Framework!"
+  ];
+
   const onShowMessageClicked = $((message:string) => alert(message));
 
   return (
     <>
-      <HelloMessage message="Hello World" courseVersion={1} onShowMessage={onShowMessageClicked}/>
-      <HelloMessage message="Welcome to this Qwik Course" courseVersion={2} onShowMessage={onShowMessageClicked}/>
-      <HelloMessage message="Learn the Qwik Framework!"  courseVersion={3} onShowMessage={onShowMessageClicked}/>
+      <HelloMessage showButton={true} message="Hello World" courseVersion={1} onShowMessage={onShowMessageClicked}/>
+      <HelloMessage showButton={false} message="Welcome to this Qwik Course" courseVersion={2} onShowMessage={onShowMessageClicked}/>
+      <HelloMessage showButton={false} message="Learn the Qwik Framework!"  courseVersion={3} onShowMessage={onShowMessageClicked}/>
 
       <button onClick$={sayHello}>Say Hello</button>
 

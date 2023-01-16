6cd89755c970f2c96260d00516073efe0fa901fe
jhades <jhades.dev@gmail.com>
2023-01-16 16:55:46 +0100

    Recording Qwik course

--- src/components/hello-message/hello-message.tsx
+++ src/components/hello-message/hello-message.tsx
@@ -2,13 +2,14 @@ import { component$, PropFunction } from "@builder.io/qwik";
 
 interface HelloMessageProps {
   message:string,
-  courseVersion?:number
+  courseVersion?:number,
+  showButton: boolean,
   onShowMessage: PropFunction<(message:string) => void>;
 }
 
 export const HelloMessage = component$<HelloMessageProps>((props) => {
 
-  const {message, courseVersion, onShowMessage} = props;
+  const {message, courseVersion, onShowMessage, showButton} = props;
 
   return (
     <div class='container'>
@@ -16,7 +17,10 @@ export const HelloMessage = component$<HelloMessageProps>((props) => {
         <>
         <h1>{message}: version {courseVersion}</h1>
 
-        <button onClick$={() => onShowMessage(message)}>Show Message</button>
+          {showButton && (
+            <button onClick$={() => onShowMessage(message)}>Show Message</button>
+          )}
+
         </>
       }
     </div>
